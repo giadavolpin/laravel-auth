110e42e61fd2f3908e0e45f800c7a590f932a32e
giadavolpin <volpingiada@gmail.com>
2023-01-11 17:38:22 +0100

    model projesct

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\Admin\DashboardController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Route::get('/dashboard', function () {
