b2bd4721e0380414a6cce98762b0e543b96ce23e
giadavolpin <volpingiada@gmail.com>
2023-01-12 17:55:02 +0100

    route dashboard sistemata

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
@@ -45,7 +46,7 @@ class ProjectController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('project.show');
     }
 
     /**
