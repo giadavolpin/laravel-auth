98ee76f02a1fbe6f73a93b7cde164fe594c78939
giadavolpin <volpingiada@gmail.com>
2023-01-22 16:02:51 +0100

    welcome.blade

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -2,11 +2,14 @@
 @section('content')
 
 <div class="jumbotron p-5 mb-4 bg-light">
+   
     <div class="container py-5">
         <h1 class="display-5 fw-bold">
             Welcome to Projects by Giada Volpin
         </h1>
 
+      
+        
        </div>
 </div>
 
